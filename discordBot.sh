273e5c43b74f26281f196dfb72114f3a1ec5d4fe
thomaspcole <thomascole@carolina.rr.com>
2019-04-04 16:54:08 -0400

    Discord message sender.

---
+++ discordBot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+################################################################################
+# A simple script to send a message to a discord server.                       #
+#                                                                              #
+#                                                                              #
+################################################################################
+
+PARAMS=""
+
+while (( "$#" )); do
+  case "$1" in
+    -u|--mention)
+      mention="<@$2>"
+      shift 2
+      ;;
+    -m|--message)
+      message="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    -*|--*=)
+      echo "Error: Unknown Flag $1" >&2
+      exit 1
+      ;;
+    *)
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+    esac
+done
+
+url="https://discordapp.com/api/webhooks/URL"
+curl -H "Content-Tyep: application/json" -X POST -d '{"content": "'$mention' '$message'"}' $url
+echo ""
